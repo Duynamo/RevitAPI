9dc027f39205979f23ed99bb01c5c7bc13acfa76
duynamo <vudinhduybm@gmail.com>
2024-04-10 23:03:20 +0700

    @

--- duynamoLibrary.py
+++ duynamoLibrary.py
@@ -11,88 +11,88 @@ def flattenTo1dList(arr):
     recursive_flatten(arr)        
     return result
 
-# def isArray(array):
-#     return "List" in obj.__class__.__name__
+def isArray(array):
+    return "List" in obj.__class__.__name__
 
-# def getArrayRank(array):
-#     if isArray(array)
-#         return 1 + max(getArrayRank(item) for item in array)
-#     else: 
-#         return 0
+def getArrayRank(array):
+    if isArray(array)
+        return 1 + max(getArrayRank(item) for item in array)
+    else: 
+        return 0
 
-# def flattenArrayByOptionals(objects, level = 1):
-#     result = []
-#     for item in objects:
-#         newRank = getArrayRank(item)
-#         if isArray(item) and newRank >= level :
-#             arr = flattenTo1dList(item)
-#             result.append(arr)
-#         else:
-#             result.append(item)
-#     return result
+def flattenArrayByOptionals(objects, level = 1):
+    result = []
+    for item in objects:
+        newRank = getArrayRank(item)
+        if isArray(item) and newRank >= level :
+            arr = flattenTo1dList(item)
+            result.append(arr)
+        else:
+            result.append(item)
+    return result
 
-# ##selectionFilter
-# class selectionFilter(ISelectionFilter):
-#     def __init__(self, category1, category2,category3, category4):
-#         self.category1 = category1
-#         self.category2 = category2
-#         self.category3 = category3
-#         self.category4 = category4
+##selectionFilter
+class selectionFilter(ISelectionFilter):
+    def __init__(self, category1, category2,category3, category4):
+        self.category1 = category1
+        self.category2 = category2
+        self.category3 = category3
+        self.category4 = category4
     
-#     def AllowElement(self, element):
-#         if element.Category.Name == self.category1 or element.Category.Name == self.category2 or element.Category.Name == self.category3 or element.Category.Name == self.category4:
-#             return True
-#         else:
-#             return False
-#     def AllowReference(reference, point):
-#         return False
+    def AllowElement(self, element):
+        if element.Category.Name == self.category1 or element.Category.Name == self.category2 or element.Category.Name == self.category3 or element.Category.Name == self.category4:
+            return True
+        else:
+            return False
+    def AllowReference(reference, point):
+        return False
     
-# ele = selectionFilter('Structural Columns', 'Structural Framing', 'Pipes','Pipe Fittings')
-# ele.copy()
+ele = selectionFilter('Structural Columns', 'Structural Framing', 'Pipes','Pipe Fittings')
+ele.copy()
 
-# ##pickObjects
-# def pickObjects():
-#     from Autodesk.Revit.UI.Selection import ObjectType
-#     refs = uidoc.Selection.PickObjects(ObjectType.Element)
-#     return  [doc.GetElement(i.ElementId) for i in refs], [ref for ref in refs]
-
-
-
-# def pickObjects():
-#     from Autodesk.Revit.UI.Selection import ObjectType
-#     refs = uidoc.Selection.PickObjects(ObjectType.Element)
-#     return  [doc.GetElement(i.ElementId) for i in refs]
-
-# lstPipes = pickObjects()
-# flat_lstPipes = [pipe for pipe in lstPipes]
-
-# pipe1 = flat_lstPipes[0]
-# pipe2 = flat_lstPipes[1]
+##pickObjects
+def pickObjects():
+    from Autodesk.Revit.UI.Selection import ObjectType
+    refs = uidoc.Selection.PickObjects(ObjectType.Element)
+    return  [doc.GetElement(i.ElementId) for i in refs], [ref for ref in refs]
 
 
-# closest_distance = float('inf')  
-# closest_connector_pipe2 = None
-# closest_connector_pipe1 = None
+
+def pickObjects():
+    from Autodesk.Revit.UI.Selection import ObjectType
+    refs = uidoc.Selection.PickObjects(ObjectType.Element)
+    return  [doc.GetElement(i.ElementId) for i in refs]
+
+lstPipes = pickObjects()
+flat_lstPipes = [pipe for pipe in lstPipes]
+
+pipe1 = flat_lstPipes[0]
+pipe2 = flat_lstPipes[1]
 
 
-# for connector1 in pipe1.ConnectorManager.Connectors:
-#     for connector2 in pipe2.ConnectorManager.Connectors:
-#         distance = connector1.Origin.DistanceTo(connector2.Origin)
-#         if distance < closest_distance:
-#             closest_distance = distance
-#             closest_connector_pipe1 = connector1
-#             closest_connector_pipe2 = connector2
+closest_distance = float('inf')  
+closest_connector_pipe2 = None
+closest_connector_pipe1 = None
+
+
+for connector1 in pipe1.ConnectorManager.Connectors:
+    for connector2 in pipe2.ConnectorManager.Connectors:
+        distance = connector1.Origin.DistanceTo(connector2.Origin)
+        if distance < closest_distance:
+            closest_distance = distance
+            closest_connector_pipe1 = connector1
+            closest_connector_pipe2 = connector2
             
-# if closest_connector_pipe1 and closest_connector_pipe2:
-# 	cor_ConnectPipe1 = closest_connector_pipe1.Origin.ToPoint()
-# 	cor_ConnectPipe2 = closest_connector_pipe2.Origin.ToPoint()
+if closest_connector_pipe1 and closest_connector_pipe2:
+	cor_ConnectPipe1 = closest_connector_pipe1.Origin.ToPoint()
+	cor_ConnectPipe2 = closest_connector_pipe2.Origin.ToPoint()
 
-# TransactionManager.Instance.EnsureInTransaction(doc)
-# pipeJoin = doc.Create.NewPipe(closest_connector_pipe1, closest_connector_pipe2)
-# TransactionManager.Instance.TransactionTaskDone()
+TransactionManager.Instance.EnsureInTransaction(doc)
+pipeJoin = doc.Create.NewPipe(closest_connector_pipe1, closest_connector_pipe2)
+TransactionManager.Instance.TransactionTaskDone()
 
 
-# OUT = cor_ConnectPipe1, cor_ConnectPipe2
+OUT = cor_ConnectPipe1, cor_ConnectPipe2
 
 #def 0002
 #to pick objects and return the picked elements list, refs and Ids
@@ -138,3 +138,49 @@ interpret = re.read()
 OUT =  interpret
 
 """__________________get All Settings Categories_________________"""
+
+#def 0004
+#to get family by name
+categories = [BuiltInCategory.OST_PipeAccessory]
+desFamTypes = []
+key = "FU_Support"
+categoriesFilter = []
+for category in categories:
+    elementTypes = FilteredElementCollector(doc).OfCategory(category).WhereElementIsElementType().ToElements()
+    for elementType in elementTypes:
+        typeName = elementType.FamilyName
+        if key in typeName:
+            desFamTypes.append(elementType)
+categoriesFilter.append(desFamTypes)
+
+flat_categoriesFilter = [ item for sublist in categoriesFilter for item in sublist]
+
+OUT = flat_categoriesFilter
+
+#def 0005
+#to get pipe location curve
+def getPipeLocationCurve(pipe):
+    # Get the location curve of the pipe
+    location_curve = pipe.Location.Curve
+    return location_curve
+
+#def 0006
+#to pick pipe only
+class selectionFilter(ISelectionFilter):
+	def __init__(self, category):
+		self.category = category
+	def AllowElement(self, element):
+		if element.Category.Name == self.category:
+			return True
+		else:
+			return False
+
+def pickPipes():
+	pipes = []
+	pipeFilter = selectionFilter("Pipes")
+
+	pipesRef = uidoc.Selection.PickObjects(Autodesk.Revit.UI.Selection.ObjectType.Element, pipeFilter,"pick Pipes")
+	for ref in pipesRef:
+		pipe = doc.GetElement(ref.ElementId)
+		pipes.append(pipe)
+	return pipes	
