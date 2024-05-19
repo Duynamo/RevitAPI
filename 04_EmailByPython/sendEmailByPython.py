66471ebd6201adb71c6c61372cf7219def06b989
duynamo <vudinhduybm@gmail.com>
2024-05-19 11:50:19 +0700

    update something

---
+++ 04_EmailByPython/sendEmailByPython.py
@@ -0,0 +1,54 @@
+import os
+from email.message import EmailMessage
+import ssl
+import smtplib
+
+emailSender = 'duyvultv@gmail.com'
+emailPassword = 'nmgx vjtz eshc tbvu'
+#emailReceiver = 'vudinhduybm@gmail.com'
+emailReceivers = ['vudinhduybm@gmail.com', 'duyvultv@gmai.com', 'vuducvubm123@gmail.com']
+emailCCList = []
+subject = 'Check my auto email'
+bodyList = ["""
+You need to send an email to FUSO customers to inform the status of the following PJ:
+PJ_1, PJ_2
+""",
+"""
+You need to send an email to FUSO customers to inform the status of the following PJ:
+PJ_14, PJ_24
+""",
+"""
+You need to send an email to FUSO customers to inform the status of the following PJ:
+PJ_12, PJ_2222
+"""]
+
+#body = "\n".join(bodyList)
+body = [body for body in bodyList]
+
+
+#region ___send to multiple Receiver with the same email
+# em = EmailMessage()
+# em['From'] = emailSender
+# em['To'] = [emailReceiver for emailReceiver in emailReceivers]
+# em['Subject'] = subject
+# em.set_content(body)
+
+# context = ssl.create_default_context()
+
+# with smtplib.SMTP_SSL('smtp.gmail.com', 465, context = context) as smtp:
+#     smtp.login(emailSender, emailPassword)
+#     smtp.sendmail(emailSender, emailReceivers, em.as_string())
+
+#endregion
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    smtp.login(emailSender, emailPassword)
+    for emailReceiver, body in zip(emailReceivers, bodyList):
+        em = EmailMessage()
+        em['From'] = emailSender
+        em['To'] = emailReceiver
+        em['Subject'] = subject
+        em.set_content(body)
+        
+        smtp.sendmail(emailSender, emailReceiver, em.as_string())
