68b3d57c4e19e44cb4a898270762f8efa8dc88bd
duynamo <vudinhduybm@gmail.com>
2024-07-18 18:36:17 +0700

    h

--- 01_pyRevitAPI/test2.py
+++ 01_pyRevitAPI/test2.py
@@ -182,7 +182,6 @@ def scaleCurve(pipe, offsetDistance):
     point2 = curve.GetEndPoint(1)
     v1 = (point1 - point2).Normalize()
     v2 = (point2 - point1).Normalize()
-
     scaled_v1 = point1.Multiply(offsetDistance)
     scaled_v2 = point2.Multiply(offsetDistance)
     offsetPoint1 = point1.Add(scaled_v1)
@@ -191,7 +190,160 @@ def scaleCurve(pipe, offsetDistance):
     return 
 
 
-pipeCurve = pipe.Location.Curve
-startPoint = pipeCurve.GetEndPoint(0)
-startPoint = pipeCurve.GetEndPoint(1)
-v1 = 
+"""Copyright by: vudinhduybm@gmail.com"""
+#region ___import all Library
+import clr
+import sys 
+import System   
+import math
+import collections
+
+from math import *
+
+clr.AddReference("ProtoGeometry")
+from Autodesk.DesignScript.Geometry import *
+
+clr.AddReference("RevitAPI") 
+import Autodesk
+from Autodesk.Revit.DB import* 
+from Autodesk.Revit.DB.Structure import*
+
+clr.AddReference("RevitAPIUI") 
+from Autodesk.Revit.UI import*
+from Autodesk.Revit.UI.Selection import ISelectionFilter
+
+clr.AddReference("System") 
+from System.Collections.Generic import List
+
+clr.AddReference("RevitNodes")
+import Revit
+clr.ImportExtensions(Revit.Elements)
+clr.ImportExtensions(Revit.GeometryConversion)
+
+clr.AddReference("RevitServices")
+import RevitServices
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
+
+clr.AddReference("System.Windows.Forms")
+clr.AddReference("System.Drawing")
+clr.AddReference("System.Windows.Forms.DataVisualization")
+
+import System.Windows.Forms 
+from System.Windows.Forms import *
+import System.Drawing
+from System.Drawing import *
+#endregion
+
+#region ___Current doc/app/ui
+doc = DocumentManager.Instance.CurrentDBDocument
+uiapp = DocumentManager.Instance.CurrentUIApplication
+app = uiapp.Application
+uidoc = DocumentManager.Instance.CurrentUIApplication.ActiveUIDocument
+view = doc.ActiveView
+#endregion
+
+#region ___someFunctions
+def uwList(input):
+    result = input if isinstance(input, list) else [input]
+    return UnwrapElement(input)
+class selectionFilter(ISelectionFilter):
+	def __init__(self, category):
+		self.category = category
+	def AllowElement(self, element):
+		if element.Category.Name == self.category:
+			return True
+		else:
+			return False
+def pickPipe():
+	pipeFilter = selectionFilter("Pipes")
+	ref = uidoc.Selection.PickObject(Autodesk.Revit.UI.Selection.ObjectType.Element, pipeFilter,"pick Pipe")
+	pipe = doc.GetElement(ref.ElementId)
+	return pipe
+	
+def getPipeParameter(p):
+    paramDiameters = []
+    paramPipingSystems = []
+    paramLevels = []
+    paramPipeTypes = []
+
+    paramDiameter = p.get_Parameter(BuiltInParameter.RBS_PIPE_DIAMETER_PARAM).AsDouble() * 304.8
+    
+    paramPipeTypeId = p.GetTypeId()
+    paramPipeType = doc.GetElement(paramPipeTypeId)
+    paramPipeTypes.append(paramPipeType)
+    pipeTypeName = paramPipeType.LookupParameter("Type Name").AsString()
+	
+    paramPipingSystemId = p.get_Parameter(BuiltInParameter.RBS_PIPING_SYSTEM_TYPE_PARAM).AsElementId()
+    paramPipingSystem = doc.GetElement(paramPipingSystemId)
+    paramPipingSystems.append(paramPipingSystem)
+    pipingSystemName = paramPipingSystem.LookupParameter("System Classification").AsValueString()
+
+    paramLevelId = p.get_Parameter(BuiltInParameter.RBS_START_LEVEL_PARAM).AsElementId()
+    paramLevel = doc.GetElement(paramLevelId)
+    paramLevels.append(paramLevel)
+
+    return paramDiameter, paramPipingSystem, paramPipeType, paramLevel
+def scaleCurve(pipe, offsetDistance):
+    k = offsetDistance/304.8
+    curve = pipe.Location.Curve
+    point1 = curve.GetEndPoint(0)
+    point2 = curve.GetEndPoint(1)
+    
+    # Compute the direction vectors and normalize them
+    v1 = (point1 - point2).Normalize()
+    v2 = (point2 - point1).Normalize()
+    
+    # Scale the vectors by the offset distance
+    scaled_v1 = v1.Multiply(k)
+    scaled_v2 = v2.Multiply(k)
+    
+    # Compute the new endpoints
+    offsetPoint1 = point1.Add(scaled_v1)
+    offsetPoint2 = point2.Add(scaled_v2)
+    
+    # Create a new line with the scaled endpoints
+    newCurve = Line.CreateBound(offsetPoint1, offsetPoint2)
+    
+    # Update the pipe's location curve
+    pipe.Location.Curve = newCurve
+
+    return newCurve    
+#endregion
+
+
+eleList   = uwList(IN[0])
+
+# Do some action in a Transaction
+TransactionManager.Instance.EnsureInTransaction(doc)
+pipe = pickPipe()
+pipeParam = getPipeParameter(pipe)
+newCurve = scaleCurve(pipe, 1000)
+
+TransactionManager.Instance.TransactionTaskDone()
+
+OUT = newCurve
+
+
+def connectElbowToPipe(doc, ele1, ele2):
+    TransactionManager.Instance.EnsureInTransaction(doc)
+    
+    # Get the connectors of the elbow and pipe
+    if ele1.Name == "Pipes":
+        ele1Conns = [conn for conn in ele1.ConnectorManager.Connectors]
+    elif ele1.Name != "Pipes":
+        ele1Conns = [conn for conn in ele1.MEPModel.ConnectorManager.Connectors]    
+    if ele2.Name == "Pipes":
+        ele2Conns = [conn for conn in ele2.ConnectorManager.Connectors]
+    elif ele2.Name != "Pipes":
+        ele2Conns = [conn for conn in ele2.MEPModel.ConnectorManager.Connectors] 
+    
+    # Find the nearest connectors between the elbow and pipe
+    nearestEle1Conn = findNearestConnector(ele1Conns, ele2Conns[0].Origin)
+    nearestEle2Conn = findNearestConnector(ele2Conns, nearestEle1Conn.Origin)
+    
+    # Connect the nearest connectors
+    nearestEle1Conn.ConnectTo(nearestEle2Conn)
+    
+    TransactionManager.Instance.TransactionTaskDone()
+    return nearestEle1Conn, nearestEle2Conn
