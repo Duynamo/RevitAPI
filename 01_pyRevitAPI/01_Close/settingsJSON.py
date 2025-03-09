7c47fa93ceb192ad1b4732e9e679f3b20106c5af
duynamo <vudinhduybm@gmail.com>
2025-03-09 15:04:53 +0700

    s

---
+++ 01_pyRevitAPI/01_Close/settingsJSON.py
@@ -0,0 +1,15 @@
+{
+    "python.linting.pylintEnabled": true,
+    "python.linting.pep8Enabled": false,
+    "python.pythonPath": "C:\\Program Files (x86)\\IronPython 2.7\\ipy.exe",
+    "python.autoComplete.extraPaths": [
+        "C:\\Users\\Laptop\\.vscode\\ironpython-stubs-master\\release\\stubs.min",
+        "C:\\Program Files\\Autodesk\\Revit 2024\\Program"  // Thư mục Revit API nếu cần
+    ],
+    "python.languageServer": "Pylance",  // Hoặc "Microsoft" nếu Pylance không hoạt động
+    "python.jediEnabled": false,
+    "python.analysis.extraPaths": [
+        "C:\\Users\\Laptop\\.vscode\\ironpython-stubs-master\\release\\stubs.min",
+        "C:\\Program Files\\Autodesk\\Revit 2024\\Program"
+    ]
+}
