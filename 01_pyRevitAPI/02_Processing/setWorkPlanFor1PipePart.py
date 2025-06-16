2bc98a852884c7539f156a039bcffc9e6a59dc7c
duynamo <vudinhduybm@gmail.com>
2025-06-16 21:09:36 +0700

    update set workplan for 1 selected part

--- 01_pyRevitAPI/02_Processing/setWorkPlanFor1PipePart.py
+++ 01_pyRevitAPI/02_Processing/setWorkPlanFor1PipePart.py
@@ -64,103 +64,55 @@ def pickPipeOrPipePart():
     return desEle
 
 selectedElement = pickPipeOrPipePart()
-selectedElement_refLevel_ID = selectedElement.get_Parameter(BuiltInParameter.RBS_START_LEVEL_PARAM).AsElementId()
-selectedElement_refLevel = doc.GetElement(selectedElement_refLevel_ID)
-selectedElement_COPLevel = selectedElement.LookupParameter("Lower End Centerline Elevation").AsDouble()
+elementCheck = selectedElement.Category.Name
 
-TransactionManager.Instance.EnsureInTransaction(doc)
+if elementCheck == 'Pipes':
+    pipe_refLevel_ID = selectedElement.get_Parameter(BuiltInParameter.RBS_START_LEVEL_PARAM).AsElementId()
+    pipe_refLevel = doc.GetElement(pipe_refLevel_ID)
+    pipe_COPLevel = selectedElement.LookupParameter("Lower End Centerline Elevation").AsDouble()
+    TransactionManager.Instance.EnsureInTransaction(doc)
+    try:
+        # Lấy tọa độ và thông tin của Level tham chiếu
+        pipe_refLevelElevation = pipe_refLevel.Elevation  # Độ cao của Level (feet)
+        pipe_newElevation = pipe_refLevelElevation + pipe_COPLevel
+        # Tạo một mặt phẳng mới
+        # Mặt phẳng nằm ngang (normal = Z-axis), gốc tại (0, 0, newElevation)
+        origin = XYZ(0, 0, pipe_newElevation)
+        normal = XYZ(0, 0, 1)  # Hướng pháp tuyến (Z-axis)
+        plane = Plane.CreateByNormalAndOrigin(normal, origin)
+        sketchPlane = SketchPlane.Create(doc, plane)
+        view.SketchPlane = sketchPlane
+        # Kết thúc giao dịch
+        TransactionManager.Instance.TransactionTaskDone()
+    except Exception as e:
+        # Hủy giao dịch nếu có lỗi
+        TransactionManager.Instance.ForceCloseTransaction()
+        TaskDialog.Show("Error", "An error occurred: {}".format(str(e)))
 
-try:
-    # Lấy tọa độ và thông tin của Level tham chiếu
-    refLevelElevation = selectedElement_refLevel.Elevation  # Độ cao của Level (feet)
+if elementCheck != 'Pipes':
+    fitting_ParamLevel = selectedElement.get_Parameter(BuiltInParameter.FAMILY_LEVEL_PARAM)
+    fitting_RefLevel = doc.GetElement(fitting_ParamLevel.AsElementId())
+    fitting_COPLevel = selectedElement.LookupParameter("Elevation from Level").AsDouble()
+    TransactionManager.Instance.EnsureInTransaction(doc)
+    try:
+        # Lấy tọa độ và thông tin của Level tham chiếu
+        fitting_refLevelElevation = fitting_RefLevel.Elevation  # Độ cao của Level (feet)
+        fitting_newElevation = fitting_refLevelElevation + fitting_COPLevel
+        # Tạo một mặt phẳng mới
+        # Mặt phẳng nằm ngang (normal = Z-axis), gốc tại (0, 0, newElevation)
+        origin = XYZ(0, 0, fitting_newElevation)
+        normal = XYZ(0, 0, 1)  # Hướng pháp tuyến (Z-axis)
+        plane = Plane.CreateByNormalAndOrigin(normal, origin)
+        sketchPlane = SketchPlane.Create(doc, plane)
+        view.SketchPlane = sketchPlane
+        # Kết thúc giao dịch
+        TransactionManager.Instance.TransactionTaskDone()
+    except Exception as e:
+        # Hủy giao dịch nếu có lỗi
+        TransactionManager.Instance.ForceCloseTransaction()
+        TaskDialog.Show("Error", "An error occurred: {}".format(str(e)))
 
-    # Tạo mặt phẳng ảo dựa trên Level
-    # Mặt phẳng sẽ nằm tại độ cao của Level + Lower End Centerline Elevation
-    newElevation = refLevelElevation + selectedElement_COPLevel
+    pass
 
-    # Tạo một mặt phẳng mới
-    # Mặt phẳng nằm ngang (normal = Z-axis), gốc tại (0, 0, newElevation)
-    origin = XYZ(0, 0, newElevation)
-    normal = XYZ(0, 0, 1)  # Hướng pháp tuyến (Z-axis)
-    plane = Plane.CreateByNormalAndOrigin(normal, origin)
-
-    # Tạo SketchPlane từ mặt phẳng ảo
-    sketchPlane = SketchPlane.Create(doc, plane)
-
-    # Đặt SketchPlane làm Work Plane cho view hiện tại
-    view.SketchPlane = sketchPlane
-
-    # Kết thúc giao dịch
-    TransactionManager.Instance.TransactionTaskDone()
-
-    # Thông báo thành công
-    # TaskDialog.Show("Success", "Work plane has been set at elevation: {} feet".format(newElevation))
-
-except Exception as e:
-    # Hủy giao dịch nếu có lỗi
-    TransactionManager.Instance.ForceCloseTransaction()
-    TaskDialog.Show("Error", "An error occurred: {}".format(str(e)))
-
-# # Bắt đầu xử lý
-# try:
-#     # Lấy đối tượng được chọn
-#     selectedElement = pickPipeOrPipePart()
-
-#     # Lấy Level tham chiếu từ tham số LevelId
-#     selectedElement_refLevel_ID = selectedElement.get_Parameter(BuiltInParameter.RBS_START_LEVEL_PARAM).AsElementId()
-#     if selectedElement_refLevel_ID == ElementId.InvalidElementId:
-#         raise Exception("No valid Level associated with the selected element.")
-#     selectedElement_refLevel = doc.GetElement(selectedElement_refLevel_ID)
-#     refLevelElevation = selectedElement_refLevel.Elevation  # Độ cao của Level (feet)
-
-#     # Xác định khoảng cách offset dựa trên loại đối tượng
-#     offsetElevation = 0.0
-#     if selectedElement.Category.Name == "Pipes":
-#         param = selectedElement.LookupParameter("Lower End Centerline Elevation")
-#         if param is None:
-#             raise Exception("Parameter 'Lower End Centerline Elevation' not found for Pipe.")
-#         offsetElevation = param.AsDouble()
-#     else:  # Pipe Fittings hoặc Pipe Accessories
-#         param = selectedElement.LookupParameter("Elevation from Level")
-#         if param is None:
-#             raise Exception("Parameter 'Elevation from Level' not found for Pipe Fitting or Accessory.")
-#         offsetElevation = param.AsDouble()
-
-#     # Tính độ cao mới cho mặt phẳng
-#     newElevation = refLevelElevation + offsetElevation
-
-#     # Bắt đầu giao dịch để tạo và chỉnh sửa mô hình
-#     TransactionManager.Instance.EnsureInTransaction(doc)
-
-#     try:
-#         # Tạo một mặt phẳng mới
-#         origin = XYZ(0, 0, newElevation)
-#         normal = XYZ(0, 0, 1)  # Hướng pháp tuyến (Z-axis)
-#         plane = Plane.CreateByNormalAndOrigin(normal, origin)
-
-#         # Tạo SketchPlane từ mặt phẳng ảo
-#         sketchPlane = SketchPlane.Create(doc, plane)
-
-#         # Kiểm tra xem view có hỗ trợ Work Plane không
-#         if not view.CanBeModified():
-#             raise Exception("Current view does not support setting a work plane.")
-
-#         # Đặt SketchPlane làm Work Plane cho view hiện tại
-#         view.SketchPlane = sketchPlane
-
-#         # Kết thúc giao dịch
-#         TransactionManager.Instance.TransactionTaskDone()
-
-#         # Thông báo thành công
-#         # TaskDialog.Show("Success", "Work plane has been set at elevation: {} feet".format(newElevation))
-
-#     except Exception as e:
-#         # Hủy giao dịch nếu có lỗi
-#         TransactionManager.Instance.ForceCloseTransaction()
-#         raise Exception("Error during plane creation: {}".format(str(e)))
-
-# except Exception as e:
-#     # Thông báo lỗi
-#     TaskDialog.Show("Error", str(e))
-
-OUT = selectedElement_COPLevel, selectedElement_refLevel
+# OUT = selectedElement_COPLevel, selectedElement_refLevel
+OUT = 'Hello World'
